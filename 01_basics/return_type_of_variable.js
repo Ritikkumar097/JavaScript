bddf01e7d583f8bfdf1406d7c356b94036ad335f
Ritikkumar097 <ritik584656@gmail.com>
2026-02-14 02:40:23 +0530

    return_type

---
+++ 01_basics/return_type_of_variable.js
@@ -0,0 +1,40 @@
+//  *********** Primitive Datatype ****************
+
+let string = "123abc"   // string datatype
+console.log("dataType of string: ",typeof string);  // string
+
+let number = 563  // number datatype
+console.log("dataType of number: ",typeof number);  // number
+
+let boolean = true  // boolean datatype
+console.log("dataType of boolean: ",typeof boolean);  // boolean
+
+let temp = null  // null datatype
+console.log("dataType of temp(null): ",typeof null);   // object
+
+let outsideTemp = undefined  // undefined datatype
+console.log("dataType of outsideTemp: ",typeof outsideTemp);  // undefined
+
+const symbol = Symbol('8645')   // Symbol datatype
+console.log("dataType of symbol: ",typeof symbol);  // symbol
+
+const bigNumber = 46549595559555555655n   // BigInt datatype
+console.log("dataType of bigNumber: ",typeof bigNumber);  // bigint
+
+
+// **************** Non Primitive datatype ************************
+
+const fruits = ["mango", "guava", "banana", "apple"]   // array datatype
+console.log("datatype of fruits: ",typeof fruits)   // object
+
+let myObj = {      // object datatype
+    name: "zanng",
+    age:56,
+}
+console.log("daataType of myObj: ",typeof myObj)   // object
+
+let myFunction = function(){   // function datatype
+    areaOfCircle = 22/7(R**2);
+}
+console.log("dataType of myFunction: ",typeof myFunction)   // function
+
