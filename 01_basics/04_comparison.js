dbe169eadf90cc1442142a4d0d5dabf6b0d4f6d2
Ritikkumar097 <ritik584656@gmail.com>
2026-02-14 01:25:46 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);  // equal
+// console.log(2!=1);  // not equal
+
+// console.log("2">1); //js converts this string into integer internally
+
+console.log(null > 0);
+console.log(null == 0); // use === instead of ==
+console.log("2" === 2); // gives false because of ===
+console.log(null >= 0);
+console.log(null <= 0);
