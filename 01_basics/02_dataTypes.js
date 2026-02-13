75dc0cd2eb9541a9e770a64fc2a17d80dbb37f5f
Ritikkumar097 <ritik584656@gmail.com>
2026-02-13 17:28:24 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all js code as newer version
+
+// alert( 3 + 3 )   // we are using nodejs , not browser
+
+console.log( 3 + 3 )
+console.log("Ritik")
+
+let name = "Ritik"
+let age = 19
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// brigint
+// string => ""
+// boolean => true/false
+// null => standalonevalue
+// undefined 
+// symbol => unique
+
+console.log(typeof age)
+
+
